bfff25e8e3ed7945aded6be342d073a0cdd20929
kabirnagpal <21kabirnagpal@gmail.com>
2019-03-13 18:22:01 +0530

    image  recognition and modeling done

--- Facial Expression/2/cvpart.py
+++ Facial Expression/2/cvpart.py
@@ -1,29 +1,33 @@
 import numpy as np
 import cv2
 import pickle
+import matplotlib.pyplot as plt
 
-with open('/home/jak/Desktop/face2', 'rb') as f:
+
+with open('/home/jak/Desktop/DL/Facial Expression/face2', 'rb') as f:
     model = pickle.load(f)
 
-
-#web cam
 cap = cv2.VideoCapture(0)
 
 if cap.isOpened():
     ret, frame = cap.read()
 else:
     ret = False
-img1=frame
+img=frame
 #img1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 cap.release()
 
-"""
-imgpath="/home/jak/Desktop/1.jpg"
-img1=cv2.imread(imgpath,0)
-plt.imshow(img1,cmap="gray")        #many attributes
-plt.show()"""
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    
+img1 = img[y:y+h, x:x+w]
 
+plt.imshow(img1)        #many attributes
+plt.show()
 
 def resize2SquareKeepingAspectRation(img, interpolation, size=48):
     h, w = img.shape[:2]
@@ -41,29 +45,27 @@ def resize2SquareKeepingAspectRation(img, interpolation, size=48):
         mask[y_pos:y_pos+h, x_pos:x_pos+w, :] = img[:h, :w, :]
     return cv2.resize(mask, (size, size), interpolation)
 
-
-
 img1 = resize2SquareKeepingAspectRation(img1, cv2.INTER_AREA)
-
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 
 array=[]
 array.insert(0,img1)
-x=np.asarray(array)
-x = x.reshape(1,48,48,1)
-p=model.predict(x)
-
+arr=np.asarray(array)
+arr = arr.reshape(1,48,48,1)
+p=model.predict(arr)
 
 mylist=np.array(p).tolist()
 ind = np.argmax(mylist)
-print(ind+1)
 
+if ind==0: a='Angry'
+elif ind==1: a='Dsigust'
+elif ind==2: a='Fear'
+elif ind==3: a='Happy'
+elif ind==4: a='Sad'
+elif ind==5: a='Surprise'
+else : a='Neutral'
 
-if ind==0: print('Angry')
-elif ind==1: print('Dsigust')
-elif ind==2: print('Fear')
-elif ind==3: print('Happy')
-elif ind==4: print('Sad')
-elif ind==5: print('Surprise')
-else :print('Neutral')
+cv2.putText(img, a, (x, y), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 0, 255), 2)
 
+plt.imshow(img)        #many attributes
+plt.show()
